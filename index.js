738c80cf5308e12e63a73dd8f6ec9a3d329adf37
BuildTools <cephas8080@outlook.com>
2022-10-17 20:23:53 +0100

    maybe final endpoint

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ const port = process.env.PORT || 3001;
 const client_id = process.env.client_id
 const bearer = process.env.bearer
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(`<h1>gamelist:</h1><br/><h2>/gamelist/{platform}/{rating}/{amount}</h2>`)
 })
-app.get('/gamelist', (req, res) => {
+app.get('/gamelist/:platforms/:rating/:amount', (req, res) => {
+  if(req.params.amount == null || req.params.platforms == null || req.params.platforms == null)res.send("{error}")
   axios({
   url: "https://api.igdb.com/v4/games",
   method: 'POST',
@@ -18,7 +19,8 @@ app.get('/gamelist', (req, res) => {
       'Client-ID': client_id,
       'Authorization': bearer,
   },
-  data: "where rating > 85 & platforms = 6; fields name,alternative_names,summary,storyline,url,first_release_date,artworks,cover,screenshots,status; "
+  data: "where rating > "+req.params.rating+" & platforms = "+req.params.platforms+" & category = 1; fields name,alternative_names.*,summary,storyline,url,first_release_date,artworks.url,cover.url,screenshots.*,status; where rating > 85 & platforms = 6; fields name,alternative_names.*,summary,storyline,url,first_release_date,artworks.url,cover,screenshots.url,status; limit "+req.params.amount+";"
+  // 85 6 10
 })
   .then(response => {
       let result = response.data;
@@ -30,4 +32,8 @@ app.get('/gamelist', (req, res) => {
       res.send(result)
   });
 })
+app.get('/*', (req, res) => {
+  res.send('error')
+})
+
 app.listen(port, () => {console.log(`starting the app on ${port}`)})
